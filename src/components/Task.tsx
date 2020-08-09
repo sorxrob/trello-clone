e69f767bc37d11c538e3fe1b6e2c64f600389963
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-08-09 16:59:47 +0800

    [refactor] Update Task border radius

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -6,12 +6,11 @@ import { TaskType } from '../initial-data';
 
 const Container = styled.div`
   border: 1px solid lightgrey;
-  border-radius: 2px;
+  border-radius: 3px;
   padding: 8px;
   margin-bottom: 8px;
   background-color: white;
   box-shadow: 0 1px 0 rgba(9, 30, 66, 0.25);
-  border-radius: 3px;
 `;
 
 const Task = ({ task, index }: { task: TaskType; index: number }) => {
